610d90736dbcbae4bbee0aa493e2a34fd94e5b79
Eli Holzman <eli770440@gmail.com>
2026-02-15 16:34:13 +0200

    update gh-pg settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    images: { unoptimized: true },
+    basePath: "/nextjs-climbing-store",
+    assetPrefix: "/nextjs-climbing-store/",
+};
 
 export default nextConfig;
