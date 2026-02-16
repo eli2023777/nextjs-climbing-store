f31d9227e64766bb4785487c9504c3116b45a03f
Eli Holzman <eli770440@gmail.com>
2026-02-16 16:34:49 +0200

    change nextConfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    output: "export",
     images: { unoptimized: true },
-    basePath: "/nextjs-climbing-store",
-    assetPrefix: "/nextjs-climbing-store/",
 };
 
 export default nextConfig;
